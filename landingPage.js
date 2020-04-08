8cc09036c1caba083e7e15b0299b132c2e3290ea
alizahir23 <alizahiruddin@gmail.com>
2020-04-08 21:17:24 +0530

    added animations with jquery

---
+++ landingPage.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $(".email-button").click(function (e) {
+        e.preventDefault();
+        $(".sign-in-buttons").hide("fast");
+        $(".email-section").show("fast");
+    })
+
+    $(".email-next").click(function (e) {
+        e.preventDefault();
+
+        if (validateEmail($(".email-input")[0])) {
+
+            $(".email-next").hide();
+            $(".email-input").hide("fast");
+            $(".email-submit").show("fast");
+            $(".password-input").show("fast");
+            $(".name-fname").show("fast");
+            $(".name-lname").show();
+        }
+
+    });
+
+    $(".email-input").keypress(function (e) {
+
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            $(".email-next").click();
+        }
+    });
+
+    $(".email-cancel").click(function (e) {
+        e.preventDefault();
+        $(".email-section").hide("fast");
+        $(".sign-in-buttons").show("fast");
+        $(".email.section").hide();
+        $(".password.input").hide();
+        $(".email-submit").hide();
+        $(".email-next").show();
+        $(".email-input")[0].value = "";
+    });
+})
+
