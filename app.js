a60b33e8f9cdf5bf6c8b54a18e28b1966e31da1b
alizahir23 <alizahiruddin@gmail.com>
2020-04-08 17:58:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,13 +36,13 @@ function existingUser(e) {
 function validateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (inputText.value.match(mailformat)) {
-        document.querySelector('.email-input').style.borderBottom = "0.5px solid #1abc9c";
+        document.querySelector('.email-input').style.borderBottom = "0.8px solid #1abc9c";
         return true;
     } else if (inputText.value === "") {
-        document.querySelector('.email-input').style.borderBottom = "0.5px solid #e74c3c";
+        document.querySelector('.email-input').style.borderBottom = "0.8px solid #e74c3c";
     }
     else {
-        document.querySelector('.email-input').style.borderBottom = "0.5px solid #e74c3c";
+        document.querySelector('.email-input').style.borderBottom = "0.8px solid #e74c3c";
         document.querySelector('.email-input').focus();
         return false;
     }
